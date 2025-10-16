415c8d798ece1100d2777d5646661f0370bf9805
Aman Kumar95 <amansd349@gmail.com>
2025-10-16 17:22:57 +0530

    Adding minor things

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -69,8 +69,9 @@ if (!avatar) {
     password,
     username:username.toLowerCase()
 })
-
+console.log("There i am")
 console.log(user);
+console.log("Hi there ")
 
  const createdUser= await User.findById(user._id).select("-password -refreshToken")
  console.log(createdUser);
@@ -85,6 +86,38 @@ return res.status(201).json(
 )
 })
 
+const loginUser= asyncHandler(async(req,res)=>{
+    // req body ->data
+    // username or email
+    // find the user
+    //password check
+    //access and referesh token 
+    //send cookie
+
+    const {email,username,password} = req.body
+
+    if (!username || !email) {
+        throw new ApiError(400,"username or email is required")
+    }
+
+   const user=  User.findOne({
+        $or:[{username},{email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404,"User Does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordValid(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401,"Invalid Credentials")
+    }
+
+})
 
 
-export {registerUser}
+
+export {registerUser,
+    loginUser
+}
