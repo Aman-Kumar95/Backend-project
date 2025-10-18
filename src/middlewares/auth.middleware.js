55b8d9bff527b5a12c7cc6835c496d0fd39543b0
Aman Kumar95 <amansd349@gmail.com>
2025-10-18 10:01:22 +0530

    Log Out User Functionality Added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,13 +4,25 @@ import { asyncHandler } from "../utils/asyncHandler";
 import jwt from "jsonwebtoken"
 
 export const verifyJWT  = asyncHandler(async(req, res,next)=>{
-  const token=   req.cookies?.accessToken ||  req.header("Authorization")?.replace("Bearer ","")
+try {
+    const token=   req.cookies?.accessToken ||  req.header("Authorization")?.replace("Bearer ","")
+  
+      if (!token) {
+          throw new ApiError(401,"Unauthorized request")
+      }
+  
+     const decodedToken=  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET) // jb hmm verify krte hain to wo 
+  
+     const user= await User.findById(decodedToken?._id).select("-password -refreshToken")
+      console.log("user hai ye ",user)
+     if (!user) {
+    throw new ApiError(401,"Invalid Access Token")
+     }
+  
+     req.user = user
+     next() 
+} catch (error) {
+  throw new ApiError(401,error?.message || "Invalid access token")
+} // it is there for when the work of one middleware done then to place it to another middleware
 
-    if (!token) {
-        throw new ApiError(401,"Unauthorized request")
-    }
-
-   const decodedToken=  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET) // jb hmm verify krte hain to wo 
-
-   const user= await User.findById(decodedToken?._id).select("-password -refreshToken")
 })
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import {registerUser} from '../controllers/user.controller.js'
+import {loginUser, logout, registerUser} from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(
@@ -16,5 +17,10 @@ maxCount: 1,
     }
    ]),
     registerUser)
+
+    router.route("/login").post(loginUser)
+
+    //secured routes
+    router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router
