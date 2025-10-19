7cea8fda2712f8c8e83d01d3680eb3cd2bd92602
Aman Kumar95 <amansd349@gmail.com>
2025-10-19 18:53:44 +0530

    refreshAccessToken endpoint

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,10 +2,12 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function(req,file,cb){  
+         console.log("🔥 Multer received:", file.fieldname);
 
         cb(null,"./public/temp")   //cb(error, value)
     },
     filename: function(req, file, cb){
+        console.log("🔥 Multer processing file:", file.originalname);
         cb(null, file.originalname)
     }
 })
